f495dc9ca0e44e2c49503d1aaa5e387abbc86ec3
Yudai Takada <t.yudai92@gmail.com>
2018-08-19 01:02:16 +0900

    Remove unnecessary space.

--- radeontop2csv.rb
+++ radeontop2csv.rb
@@ -4,15 +4,15 @@ def valid_argument?
   ARGV.size() == 1 and File.exist?(ARGV[0])
 end
 
-def get_time (src)
+def get_time(src)
   return Time.at(src[/.*?:/].gsub(/:/, '').to_i).to_s
 end
 
-def format_line (line)
+def format_line(line)
   return line.gsub(/(\s|　|[a-zA-Z%])+/, '').gsub(/.*?:/, get_time(line) + ',')
 end
 
-def convert (target)
+def convert(target)
   File.open('result.csv','w') do |result_file|
     result_file.puts('Time,Graphics pipe,Event Engine,Vertex Grouper + Tesselator,Texture Addresser,Shader Export,Sequencer Instruction Cache,Shader Interpolator,Scan Converter,Primitive Assembly,Depth Block,Color Block')
     File.open(target) do |file|
